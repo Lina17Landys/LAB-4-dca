7b1c8bc4fa56c5250eae60cf1d9b152dcc4a133c
Lina Marcela Landys Samboni <lina_landys@MacBook-Pro-de-Lina.local>
2023-09-24 16:34:48 -0500

    imprimen

--- src/components/recipecards.ts
+++ src/components/recipecards.ts
@@ -1,4 +1,3 @@
-import { recipies } from "../index";
 class RecipeCards extends HTMLElement {
     constructor() {
         super();
@@ -6,18 +5,43 @@ class RecipeCards extends HTMLElement {
     }
 
     connectedCallback() {
-        this.render();
+        this.fetchDataAndRender();
     }
 
-   render(){
-        this.shadowRoot!.innerHTML = `
-        <link rel="stylesheet" href="../main.css">
+    async fetchDataAndRender() {
+        try {
+            const response = await fetch("https://www.themealdb.com/api/json/v1/1/filter.php?c=Dessert");
+            const data = await response.json();
+            const meals = data.meals || [];
 
-            
-        `;
+            const recipeList = document.createElement("ul");
+
+            meals.forEach((meal: { strMeal: string, strMealThumb: string }) => {
+                const recipeItem = document.createElement("li");
+                const recipeImage = document.createElement("img");
+                recipeImage.src = meal.strMealThumb; // URL de la imagen
+                recipeImage.alt = meal.strMeal; // Texto alternativo para la imagen
+                recipeItem.appendChild(recipeImage);
+                
+                const recipeName = document.createElement("p");
+                recipeName.textContent = meal.strMeal; // Nombre de la receta
+                recipeItem.appendChild(recipeName);
+
+                recipeList.appendChild(recipeItem);
+            });
+
+            this.shadowRoot!.innerHTML = `
+            <link rel="stylesheet" href="../main.css">
+
+                <h2>Desserts recipes</h2>
+            `;
+            this.shadowRoot!.appendChild(recipeList);
+        } catch (error) {
+            console.error("Error al obtener datos de la API:", error);
+        }
     }
 }
 
-
 customElements.define("recipe-container", RecipeCards);
 export default RecipeCards;
+
